f621bbd79fecaacb262afac0cd26890720f32bda
Heather Kolinsky <146090360+HeatherKolinskyBSU@users.noreply.github.com>
2024-10-24 13:07:45 +0100

    Ex1-MathsQuiz
    Added a way of running the program multiple times without needing to entirely restart the program.

--- Ex1-MathsQuiz/Ex1-MathsQuiz.cpp
+++ Ex1-MathsQuiz/Ex1-MathsQuiz.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main (){
-     int displayMenu();{ // initialises the displayMenu function
-                         // which contains the main manu text and input
+     programStart:
+     void displayMenu();{ // initialises the displayMenu function
+                          // which contains the main manu text and input
          int menuOption = 0; // ensures the user's choice is reset on program start
          cout << "Maths Quiz" << "\n"
               << "----------" << "\n"
@@ -16,5 +17,21 @@ int main (){
          // asks for user input and waits to receive one
     }
 
-     return 0; //return statement
+
+    char runAgain;
+    cout << endl
+         << " Would you like to run another test? [Y/N]\n"
+         << "(Any input other than 'Y' or 'y' exits the program)" << endl;
+    cin >> runAgain;
+    // asks for user input then waits for response
+
+    while (runAgain == 'Y' || runAgain == 'y') {
+        goto programStart;
+        // goto sucks but the program is small enough for me to get away with it
+    }
+    cout << "Exiting program..." << endl;
+    // while loop checks if the user wants to re-run the program
+    // if not, program ends
+
+     return 0;
 }
