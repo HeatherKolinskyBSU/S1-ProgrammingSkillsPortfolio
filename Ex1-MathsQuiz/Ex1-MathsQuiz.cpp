dd77b87647ca1902242ca986d6a40db7aff57269
Heather Kolinsky <146090360+HeatherKolinskyBSU@users.noreply.github.com>
2024-10-24 12:47:03 +0100

    Ex1-MathsQuiz
    Comments update

--- Ex1-MathsQuiz/Ex1-MathsQuiz.cpp
+++ Ex1-MathsQuiz/Ex1-MathsQuiz.cpp
@@ -1,16 +1,16 @@
-#include <iostream> // include iostream
-using namespace std; // declares standard namespace
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main (){
      int displayMenu();{ // initialises the displayMenu function
-                         // which contains the main manu text
+                         // which contains the main manu text and input
          int menuOption = 0; // ensures the user's choice is reset on program start
          cout << "Maths Quiz" << "\n"
               << "----------" << "\n"
               << "[Easy]         - 1" << "\n"
               << "[Intermediate] - 2" << "\n"
               << "[Hard]         - 3" << "\n"
-              // provides the list of options that the user can choose from
               << "Choose a difficulty level:" << endl;
          cin >> menuOption;
          // asks for user input and waits to receive one
