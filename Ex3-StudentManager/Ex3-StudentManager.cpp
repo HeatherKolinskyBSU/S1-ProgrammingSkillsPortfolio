79109a7770b84cf745f0691e5a6e1393a4bdf37a
Heather Kolinsky <146090360+HeatherKolinskyBSU@users.noreply.github.com>
2024-11-26 16:35:32 +0000

    Switch skeleton
    Added a basic (empty) switch statement to hold the rest of the program

--- Ex3-StudentManager/Ex3-StudentManager.cpp
+++ Ex3-StudentManager/Ex3-StudentManager.cpp
@@ -5,17 +5,52 @@ using namespace std;
 
 void mainMenu() {
     cout << "============================================" << "\n"
-        << "                    Menu" << "\n"
-        << "============================================" << "\n"
-        << "1. View all student records" << "\n"
-        << "2. View individual student record" << "\n"
-        << "3. Show student with highest total score" << "\n"
-        << "4. Show student with lowest total score" << endl;
+         << "                    Menu" << "\n"
+         << "============================================" << "\n"
+         << "1. View all student records" << "\n"
+         << "2. View individual student record" << "\n"
+         << "3. Show student with highest total score" << "\n"
+         << "4. Show student with lowest total score" << endl;
+}
+
+int menuOptions() {
+    enum path {
+        allStudents = 1,
+        oneStudent = 2,
+        highestStudent = 3,
+        lowestStudent = 4
+    };
+    cin >> path;
+
+    switch (path) {
+        case allStudents:
+            // code block
+            break;
+
+        case oneStudent:
+            // code block
+            break;
+
+        case highestStudent:
+            // code block
+            break;
+
+        case lowestStudent:
+            // code block
+            break;
+
+        default:
+            cout << "\n"
+                 << "Unable to open file. Exiting program." << endl;
+            exit()
+    }
 }
 
 int main() {
 
     mainMenu();
+
+    menuOptions();
 
     return 0;
 }
